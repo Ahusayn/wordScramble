d0e30eb2576b311e30d619cbb9ff127004217f52
Abdul <ahusaynn@gmail.com>
2024-09-11 15:27:41 +0300

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by mac on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
